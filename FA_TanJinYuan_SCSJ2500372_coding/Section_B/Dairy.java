ac36a2aa62f2215e2724bd47cf504f0a1eb99ae7
Peter060818 <jinyuan54321@gmail.com>
2025-12-16 11:32:58 +0800

    Add files via upload

---
+++ FA_TanJinYuan_SCSJ2500372_coding/Section_B/Dairy.java
@@ -0,0 +1,24 @@
+package Section_B;
+
+public class Dairy {
+	String time;
+	String note;
+	public void question1 (String t, String n) {
+		time = t;
+		note = n;
+		
+		public void show{
+			System.out.println("Reminder at" + t );
+			System.out.println("Note:" + n);
+			System.out.println();
+		
+			public static void main() {
+				Dairy d = new Dairy("10AM", "Drink water");
+				Dairy e = new Dairy("12AM", "Lunch");
+				d.show();
+				e.show();
+			}
+			
+		}
+	}
+}
---
+++ FA_TanJinYuan_SCSJ2500372_coding/Section_B/Reminder.java
@@ -0,0 +1,35 @@
+package Section_B;
+
+public class Reminder {
+	String time;
+	String description;
+	
+	public void reminder(String t, String d) {
+		time = t;
+		description = d;
+		
+	public void show{
+		System.out.println("Reminder at" + t);
+		System.out.println("Description at" + d);
+		System.out.println();
+		
+		public static void main() {
+			Reminder w = new Reminder("consist of water");
+			Reminder m = new Reminder("consist of medicine");
+			Reminder e = new Reminder("consist of exercise");
+			
+	public static void main() {
+		System.out.println("Add Reminder");
+		System.out.println("View All Reminders");
+		System.out.println("Exit");
+		System.out.println();
+	}
+			}
+		}
+	}
+
+	
+	
+}
+	
+
